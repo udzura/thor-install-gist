579c6bc2dee4f6dd628b42f5aae63531dd14ef1c
Uchio KONDO <udzura@udzura.jp>
2012-12-17 14:17:13 +0900

    Fix hook to prevend invocation on other gems uninstalled

--- lib/thor-install-gist/hook.rb
+++ lib/thor-install-gist/hook.rb
@@ -8,16 +8,18 @@ Gem.post_install do |installed|
 end
 
 Gem.pre_uninstall do |uninstalled|
-  $stderr.puts "[I] uninstalling thor tasks for thor-install-gist"
-  begin
-    Thor::Runner.new.uninstall('install-gist')
-  rescue Thor::Error => e
-    if e.message =~ /Can't find module/
-      $stderr.puts "[W] uninstalling thor tasks failed! you just have uninstalled by yourself?"
-      $stderr.puts "[W] skip uninstall"
-      true
-    else
-      raise e
+  if uninstalled.instance_variable_get(:@gem) == "thor-install-gist"
+    $stderr.puts "[I] uninstalling thor tasks for thor-install-gist"
+    begin
+      Thor::Runner.new.uninstall('install-gist')
+    rescue Thor::Error => e
+      if e.message =~ /Can't find module/
+        $stderr.puts "[W] uninstalling thor tasks failed! you just have uninstalled by yourself?"
+        $stderr.puts "[W] skip uninstall"
+        true
+      else
+        raise e
+      end
     end
   end
 end
